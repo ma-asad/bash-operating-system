46743f7c61d3a486b9e026ca3aff74be1d2b02e8
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2023-05-28 13:03:35 +0400

    resize dialog boxes

--- main/sys_info.sh
+++ main/sys_info.sh
@@ -3,14 +3,14 @@
 # Infinite loop.
 while true; do
     # Display a list of system info options to user. The selected choice is stored in the "choice" variable.
-    choice=$(yad --list --text "Select a system info option" --radiolist --column "Pick" --column "Options" FALSE "Operating System Type" FALSE "CPU Info" FALSE "Memory Info" FALSE "Hard Disk Info" FALSE "File System Info" FALSE "Exit" --separator="" --width=500 --height=500 --fontname="Sans 14" --center --print-column=2)
+    choice=$(yad --list --text "Select a system info option" --radiolist --column "Pick" --column "Options" FALSE "Operating System Type" FALSE "CPU Info" FALSE "Memory Info" FALSE "Hard Disk Info" FALSE "File System Info" FALSE "Exit" --separator="" --width=500 --height=300 --fontname="Sans 14" --center --print-column=2)
 
     # Based on user's choice, perform the respective action.
     case $choice in
         "Operating System Type")
             # "uname -o" command to get the operating system type.
             os=$(uname -o)
-            yad --info --text "Operating System: $os" --width=500 --height=500 --fontname="Sans 14" --center
+            yad --info --text "Operating System: $os" --width=500 --height=100 --fontname="Sans 14" --center
             ;;
         "CPU Info")
             # "lscpu" command to get CPU information.
@@ -20,12 +20,12 @@ while true; do
         "Memory Info")
             # "free -h" command to get memory information.
             mem_info=$(free -h)
-            yad --info --text "Memory Information: $mem_info" --width=500 --height=500 --fontname="Sans 14" --center
+            yad --info --text "Memory Information: $mem_info" --width=500 --height=200 --fontname="Sans 14" --center
             ;;
         "Hard Disk Info")
             # "df -h" command to get hard disk information.
             disk_info=$(df -h)
-            yad --info --text "Hard Disk Information: $disk_info" --width=500 --height=500 --fontname="Sans 14" --center
+            yad --info --text "Hard Disk Information: $disk_info" --width=500 --height=200 --fontname="Sans 14" --center
             ;;
         "File System Info")
             # "mount | column -t" command to get file system information.
